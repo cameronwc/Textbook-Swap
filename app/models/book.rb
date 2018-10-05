029df734801784d429eac14b3c77f3c0304e750b
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-05 10:23:44 -0600

    added book validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+    validates :isbn, presence: true, length: {minimum:9 maximum: 13 }
+    validates :title, presence: true
+    validates :edition, presence: true
+    validates :condition, presence: true
+    validates :price, presence: true
+    validates :author, presence: true
+    validates :seller_id, presence: true
+    # validates :picture_path, presence: true
 end
