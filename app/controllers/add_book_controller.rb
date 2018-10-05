71a4b6f36846372c3c268c54464c7336f381eece
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-05 13:11:40 -0600

    added controller handling

--- app/controllers/add_book_controller.rb
+++ app/controllers/add_book_controller.rb
@@ -1,4 +1,8 @@
 class AddBookController < ApplicationController
   def index
   end
+
+  def create
+    p params
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'add_book', to: 'add_book#index'
   root 'home#index'
+  get 'add_book', to: 'add_book#index'
+  post '/add_book', to: 'add_book#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
