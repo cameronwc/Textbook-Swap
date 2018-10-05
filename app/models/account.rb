40f0599668443ae1471a988da6f46a9d27a5ee6b
Sergio Orozco <sergioorozco@Sergios-MacBook-Pro.local>
2018-10-05 10:14:52 -0600

    Database working

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,6 @@ class Account < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.edu+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: true }
 
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
