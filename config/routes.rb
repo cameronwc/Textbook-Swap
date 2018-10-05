59f198ff0e25b574fc4cf7063f1169aab4234e58
Sergio Orozco <sergioorozco@Sergios-MacBook-Pro.local>
2018-10-05 13:36:05 -0600

    Create account

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'login', to: 'login#index'
-  post 'login', to: 'login#create'
+  post 'create', to: 'login#create'
 
 end
