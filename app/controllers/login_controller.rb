a4fcede98a93773e3a17975a8a2ae46059f212dc
Sergio Orozco <sergioorozco@Sergios-MacBook-Pro.local>
2018-10-05 11:55:23 -0600

    For josh to pull

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,7 @@
 class LoginController < ApplicationController
+    
     def index
-
+        #@user = Account.new(name: params[name], email: params[email],password: params[password], password_confirmation: params[password_confirmation])
+        #@user.save
     end
 end
