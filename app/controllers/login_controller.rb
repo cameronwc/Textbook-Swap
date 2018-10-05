f24a5e919019167f97fc10a6032424d31545e9fc
Sergio Orozco <sergioorozco@Sergios-MacBook-Pro.local>
2018-10-05 13:26:17 -0600

    Able to save to database

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,12 +1,12 @@
 class LoginController < ApplicationController
     def create
         p params
-        @user = Account.new(:name => params[:Account])
-        #if @user.valid?
-         #  @user.save
-        #else
-         #   p "ERROR-------------------------------------------------"
-        #end
+        @user = Account.new(:name => params[:name], :email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
+        if @user.valid?
+           @user.save
+        else
+           p "ERROR-------------------------------------------------"
+        end
     end
     def index
         
