8fc297d315de33f9e99291766448ec440bebc462
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-07 14:56:20 -0600

    added related book rendering

--- app/controllers/add_book_controller.rb
+++ app/controllers/add_book_controller.rb
@@ -5,6 +5,7 @@ class AddBookController < ApplicationController
   def create
     @createdBook = Book.create(:isbn => params['isbn'], :title => params['title'], :edition => params['edition'], :condition => params['condition'], :price => params['price'])
     @createdBook.save!
+    redirect_to "/"
   end
 
   def similar
