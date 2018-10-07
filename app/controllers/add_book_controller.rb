1e5522bf6003f93b4833002906d306551badf0bb
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-07 14:21:23 -0600

    added related book searching

--- app/controllers/add_book_controller.rb
+++ app/controllers/add_book_controller.rb
@@ -6,4 +6,10 @@ class AddBookController < ApplicationController
     @createdBook = Book.create(:isbn => params['isbn'], :title => params['title'], :edition => params['edition'], :condition => params['condition'], :price => params['price'])
     @createdBook.save!
   end
+
+  def similar
+    @returnedBooks = Book.where(:isbn => params['isbn']).limit(params['limit'])
+    puts @returnedBooks
+    render :json => @returnedBooks.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :entries, defaults: { format: 'json' }
+  
   root 'home#index'
   get 'add_book', to: 'add_book#index'
+  get 'add_book/similar', to: 'add_book#similar'
   post '/add_book', to: 'add_book#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
