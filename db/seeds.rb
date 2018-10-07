b1630f89065b483737004f911552f1061ce92687
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-07 15:06:48 -0600

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Books = Book.create({ isbn: '1584885114'}, { name: 'Advanced University Physics, Second Edition' }, { price: 100})
