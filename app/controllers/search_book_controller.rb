b311d00967e47e3f9bdcd3f1794af41fdc3714f4
Jael Butler <jbutler6@uccs.edu>
2018-10-07 18:57:43 +0000

    updated ui

--- app/controllers/search_book_controller.rb
+++ app/controllers/search_book_controller.rb
@@ -3,10 +3,8 @@ class SearchBookController < ApplicationController
 	  @text = params["search_text"]
 	  @found_books = Book.where("title like '%#{@text}%' OR author like '%#{@text}%' OR isbn like '%#{@text}%'")
 	  @found_num = @found_books.length
-	  if(@found_num <= 0)
-		  @message = "No books were found."
-	  else
-		  @message = "#{@found_num} books were found."
-	  end
+	  @message = "#{@found_num} books were found for search '#{@text}.'"
+	  
+	  
   end
 end
