51b20e3f2d34063b94f811cf6898a8a545104585
Jael Butler <jbutler6@uccs.edu>
2018-10-07 16:40:31 +0000

    no longer exact text search

--- app/controllers/search_book_controller.rb
+++ app/controllers/search_book_controller.rb
@@ -1,7 +1,7 @@
 class SearchBookController < ApplicationController
   def index
 	  @text = params["search_text"]
-	  @found_books = Book.where("title='#{@text}' OR author='#{@text}' OR isbn='#{@text}'")
+	  @found_books = Book.where("title like '%#{@text}%' OR author like '%#{@text}%' OR isbn like '%#{@text}%'")
 	  @found_num = @found_books.length
 	  if(@found_num <= 0)
 		  @message = "No books were found."
