799e5c1153bc08fb1184defb5817801c5f07ce97
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-13 17:20:38 -0600

    added crud routes

--- spec/controllers/textbook_wishlist_controller_spec.rb
+++ spec/controllers/textbook_wishlist_controller_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe TextbookWishlistController, type: :controller do
       post :create
       expect(response).to have_http_status(:success)
   end
+  describe "POST #destroy" do
+    it "returns http success" do
+      post :destroy
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
