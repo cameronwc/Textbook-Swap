6e9842cb07af24d19853fff2a97bd5346c89ad2d
EjRuacho <36456555+EjRuacho@users.noreply.github.com>
2018-10-13 17:21:52 -0600

    Adding outline to start writing the Rspec steps

--- spec/models/search_book_test_spec.rb
+++ spec/models/search_book_test_spec.rb
@@ -3,34 +3,56 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
 
   before(:each) do
-    @grinch_book = Book.new(isbn: "1665544332211", title: "Grinch Stole Christmas", edition: "12th", condition: "new", price: 100, author: "Seuss", seller_id: 104).save
-    @physics_book = Book.new(isbn: "1234567891234", title: "University Physics", edition: "12th", condition: "new", price: 100, author: "Milazzo", seller_id: 104).save
+    @grinch_book = Book.new(isbn: "1665544332211", title: "Grinch Stole Christmas", edition: "12th", condition: "new", price: 100, author: "Seuss", seller_id: "Jsmith@uccs.edu").save
+    @physics_book = Book.new(isbn: "1234567891234", title: "University Physics", edition: "12th", condition: "new", price: 100, author: "Milazzo", seller_id: "Klamer@ppcc.edu").save
   end
 
-  it 'should find results with partial title' do
-    @found_books = Book.where("title like '%Grinch%'")
-    expect(@found_books.length > 0).to be true
+  context 'Basic search functionality' do
+    it 'should find results with partial title' do
+      @found_books = Book.where("title like '%Grinch%'")
+      expect(@found_books.length > 0).to be true
+    end
+
+    it 'should find results through exact ISBN search' do
+      @found_books = Book.where("isbn='1665544332211'")
+      expect(@found_books.length > 0).to be true
+    end
+
+    it 'should find results through full title search' do
+      @found_books = Book.where("title like '%University Physics%'")
+      expect(@found_books.length > 0).to be true
+    end
+
+    it 'should find results through Author search' do
+      @found_books = Book.where("author like '%Milazzo%'")
+      expect(@found_books.length > 0).to be true
+
+    end
+
+    it 'shouldnt find results with some searches' do
+      @found_books = Book.where("title like '%idek%' OR author like '%Eric%' OR isbn like '%00000000000%'")
+      expect(@found_books.length == 0).to be true
+    end
+
   end
 
-  it 'should find results through exact ISBN search' do
-    @found_books = Book.where("isbn='1665544332211'")
-    expect(@found_books.length > 0).to be true
-  end
+  context 'Search functionality by University and basic search' do
 
-  it 'should find results through full title search' do
-    @found_books = Book.where("title like '%University Physics%'")
-    expect(@found_books.length > 0).to be true
-  end
+    it 'first' do
 
-  it 'should find results through Author search' do
-    @found_books = Book.where("author like '%Milazzo%'")
-    expect(@found_books.length > 0).to be true
+    end
 
-  end
+    it 'second' do
 
-  it 'shouldnt find results with some searches' do
-    @found_books = Book.where("title like '%idek%' OR author like '%Eric%' OR isbn like '%00000000000%'")
-    expect(@found_books.length == 0).to be true
+    end
+
+    it 'third' do
+
+    end
+
+    it 'fourth' do
+
+    end
   end
 
 end
