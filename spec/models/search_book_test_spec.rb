e1332ec32ff751f562f96a31fc217c1c4cfd6e46
Jael Butler <jbutler6@uccs.edu>
2018-10-13 23:38:37 +0000

    new rspec university search tests

--- spec/models/search_book_test_spec.rb
+++ spec/models/search_book_test_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
 
   before(:each) do
-    @grinch_book = Book.new(isbn: "1665544332211", title: "Grinch Stole Christmas", edition: "12th", condition: "new", price: 100, author: "Seuss", seller_id: "Jsmith@uccs.edu").save
-    @physics_book = Book.new(isbn: "1234567891234", title: "University Physics", edition: "12th", condition: "new", price: 100, author: "Milazzo", seller_id: "Klamer@ppcc.edu").save
+    @grinch_book = Book.new(isbn: "1665544332211", title: "Grinch Stole Christmas", edition: "12th", condition: "new", price: 100, author: "Seuss", seller_id: "jsmith@uccs.edu").save
+    @physics_book = Book.new(isbn: "1234567891234", title: "University Physics", edition: "12th", condition: "new", price: 100, author: "Milazzo", seller_id: "klamer@ppcc.edu").save
   end
 
   context 'Basic search functionality' do
@@ -38,12 +38,14 @@ RSpec.describe Book, type: :model do
 
   context 'Search functionality by University and basic search' do
 
-    it 'first' do
-
+    it 'should find results through university search' do
+	    @found_books = Book.where("seller_id like '%@uccs.edu'")
+       expect(@found_books.length > 0).to be true
     end
 
-    it 'second' do
-
+    it 'should find results through university and title search' do
+	    @found_books = Book.where("seller_id like '%@ppcc.edu' AND title like '%Grinch%'")
+	    expect(@found_books.length >0).to be true
     end
 
     it 'third' do
