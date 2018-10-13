ed3b7ea09741c1ce55eca824ec19776ceddf329b
Sergio Orozco <sergioorozco@Sergios-MacBook-Pro.local>
2018-10-13 12:11:48 -0600

    First test created

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe DashboardController, type: :controller do
 
   describe "GET #index" do
     it "returns correct owned books for given user" do
-      
+      @user = Account.new(:name => "Sergio", :email => "test@uccs.edu", :password => "password", :password_confirmation => "password")
+      @createdBook = Book.create(:isbn => "123456789", :title => "Test", :edition => "1", :author => "test" :condition => "Good", :price => "10", :seller_id => session[:user_id])
+      expect(@user.OwnedBooks).to eq "Test"
     end
   end
 
