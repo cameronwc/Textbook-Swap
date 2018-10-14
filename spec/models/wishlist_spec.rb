41f5b801489e5386880a951c0a611d5b18dc9147
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-14 10:21:56 -0600

    added 2 arguments test

--- spec/models/wishlist_spec.rb
+++ spec/models/wishlist_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe Wishlist, type: :model do
       @wishlist = Wishlist.new(user_id: 12, book_id: 2)
       expect(@wishlist.valid?).to be true
     end
-    it 'should not be defined' do
+
+    it 'should not be defined for a negative user id' do
       @Wishlist = Wishlist.new(user_id: -1, book_id: 1)
+      expect(@wishlist.valid?).to be false
+    end
+
+    it 'should not be defined without 2 arguments' do
+      @Wishlist = Wishlist.new(user_id: 1)
       expect(@wishlist.valid?).to be false
     end
 end
