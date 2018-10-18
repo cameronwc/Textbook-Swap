d07e3d3871bf712482bd39562a52bd137555e889
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-10-18 14:27:22 -0600

    made wishlist show up in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,13 @@ class DashboardController < ApplicationController
   end
 
   def index
+    @wishlistBooks_Final = []
+    @current_user = session['user_id']
+
+    @wishlistBooks = Wishlist.where(:user_id => session['user_id'])
+    @wishlistBooks.each do |wishlistBook|
+      @Bookinfo = Book.where(:id => wishlistBook.book_id)
+      @wishlistBooks_Final.push(@Bookinfo[0])
+    end
   end
 end
--- app/controllers/textbook_wishlist_controller.rb
+++ app/controllers/textbook_wishlist_controller.rb
@@ -1,15 +1,4 @@
 class TextbookWishlistController < ApplicationController
-  def index
-    @wishlistBooks_Final = []
-    @current_user = session['user_id']
-
-    @wishlistBooks = Wishlist.where(:user_id => session['user_id'])
-    @wishlistBooks.each do |wishlistBook|
-      @Bookinfo = Book.where(:id => wishlistBook.book_id)
-      @wishlistBooks_Final.push(@Bookinfo[0])
-    end
-  end
-
   def create
     @wishlistCheck = Wishlist.where(:user_id => params['user_id'], :book_id => params['book_id'])
     if(@wishlistCheck.length == 0)
