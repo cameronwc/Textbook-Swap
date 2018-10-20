05bdfe4e00e5ace21e3c363d10847b09d27b23ac
Jael Butler <jbutler6@uccs.edu>
2018-10-20 22:21:53 +0000

    updated controller

--- app/controllers/search_book_controller.rb
+++ app/controllers/search_book_controller.rb
@@ -1,11 +1,23 @@
 class SearchBookController < ApplicationController
   def index
 	  @text = params["search_text"]
-	  
+	  @university = params["search_university"] 
 
-	  @found_books = Book.where("LOWER(title) like LOWER('%#{@text}%') OR LOWER(author) like LOWER('%#{@text}%') OR isbn='#{@text}'")
+	  if(@university == "")
+	  	@found_books = Book.where("(LOWER(title) like LOWER('%#{@text}%') OR LOWER(author) like LOWER('%#{@text}%') OR isbn='#{@text}')")
+	  else
+		  @found_books = Book.joins(:seller).where("(LOWER(title) like LOWER('%#{@text}%') OR LOWER(author) like LOWER('%#{@text}%') OR isbn='#{@text}') AND (LOWER(email) like LOWER('%@#{@university}.edu'))")
+	  end
+
 	  @found_num = @found_books.length
-	  @message = "#{@found_num} books were found for search '#{@text}.'"
+	  @message = "#{@found_num} books were found"
+
+	  if(@text != "")
+		  @message = "#{@message} for search '#{@text}'"
+	  end
+	  if(@university != "")
+		  @message = "#{@message} at university '#{@university}'"
+	  end
 	  
 	  
   end
