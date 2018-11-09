fca8e35e778075cae09f06009dfa3eb38f27dbda
Josh Willis <jwillis2@uccs.edu>
2018-11-09 10:35:56 -0700

    added contact_info_display function to protect login info

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,4 +1,6 @@
+require 'sanitize'
 module LoginHelper
+  #require 'sanitize'
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -15,4 +17,55 @@ module LoginHelper
       return true
     end
   end
+  def compare_users(book_seller)
+    @current_user = current_user
+    seller_domain = book_seller.email[/(?<=@)[^.]+(?=\.)/,1]
+    if !@current_user.nil?
+    buyer_domain = @current_user.email[/(?<=@)[^.]+(?=\.)/,1]
+    end
+    if seller_domain == buyer_domain
+      return true
+    else
+      return false
+    end
+  end
+  def contact_info_display(compare)
+    format1='<a href=\"mailto:<%= book.seller.email%>\">Email Seller: <%=book.seller.email%></a>'
+    format2='<p>Not same university - no contact info displayed</p>'
+    format3='<a href=\"/Login\">Please login to view email</a>'
+
+    if logged_in? && compare
+      return Sanitize.fragment(format1, Sanitize::Config::RELAXED)
+    elsif logged_in? && !compare
+      return Sanitize.fragment(format2, Sanitize::Config::RELAXED)
+    else
+      return Sanitize.fragment(format3, Sanitize::Config::RELAXED)
+    end
+  end
+
+  # def html_message(compare)
+  #   message1="Email Seller: <%=book.seller.email%>"
+  #   message2="Not same university - no contact info displayed"
+  #   message3="Login to view contact info"
+  #
+  #   if logged_in? && compare
+  #     return message1.sanitize
+  #   elsif logged_in? && !compare
+  #     return message2.sanitize
+  #   else
+  #     return message3.sanitize
+  #   end
+  #
+  # end
+  # def html_close_tag(compare)
+  #
+  #   close_tag1 ="a"
+  #   close_tag2 ="p"
+  #
+  #   if compare || !logged_in?
+  #     return close_tag1.sanitize
+  #   else
+  #     return close_tag2.sanitize
+  #   end
+  # end
 end
--- spec/helpers/login_helper_spec.rb
+++ spec/helpers/login_helper_spec.rb
@@ -13,3 +13,27 @@ require 'rails_helper'
 RSpec.describe LoginHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# before(:each) do
+#   @ppcc_user = Account.new(name: "ppcc user", email: "user@ppcc.edu",password: "foobar", password_confirmation: "foobar")
+#   @uccs_user = Account.new(name: "uccs user", email: "user@uccs.edu",password: "foobar1", password_confirmation: "foobar1")
+#   @ppcc_user2 = Account.new(name: "ppcc user", email: "user@ppcc.edu",password: "foobar2", password_confirmation: "foobar2")
+#   @uccs_user2 = Account.new(name: "uccs user", email: "user@uccs.edu",password: "foobar3", password_confirmation: "foobar3")
+#   @grinch_book = Book.new(isbn: "1665544332211", title: "Grinch Stole Christmas", edition: "12th", condition: "new", price: 100, author: "Seuss")
+#   @physics_book = Book.new(isbn: "1234567891234", title: "University Physics", edition: "12th", condition: "new", price: 100, author: "Milazzo")
+#   @ppcc_user.books << @grinch_book
+#   @grinch_book.seller = @ppcc_user
+#   @ppcc_user.save!
+#   @grinch_book.save!
+#   @uccs_user.books << @physics_book
+#   @physics_book.seller = @uccs_user
+#   @uccs_user.save!
+#   @physics_book.save!
+# end
+#
+# describe "GET #contact_info" do
+#   it "compare " do
+#     post :, params: {'book_id' => @grinch_book}
+#     expect(response).to eq(@ppcc_user.id)
+#   end
+# end
