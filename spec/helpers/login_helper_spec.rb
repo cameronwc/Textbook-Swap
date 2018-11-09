fee6c5e548b3a34544771ea897f6a8e6ae5563b8
FezTheImmigrant <sergioorozco@Sergios-MacBook-Pro.local>
2018-11-09 13:38:14 -0700

    Testing for contact_info_display function completed

--- spec/helpers/login_helper_spec.rb
+++ spec/helpers/login_helper_spec.rb
@@ -18,10 +18,17 @@ before(:each) do
   @uccs_user = Account.new(name: "uccs user", email: "user@uccs.edu",password: "foobar1", password_confirmation: "foobar1")
   @uccs_user_1 = Account.new(name: "uccs user 1", email: "user1@uccs.edu",password: "foobar", password_confirmation: "foobar")
   @uccs_user_2 = Account.new(name: "uccs user 2", email: "user2@uccs.edu",password: "foobar1", password_confirmation: "foobar1")
+  @grinch_book = Book.new(isbn: "1665544332211", title: "Grinch Stole Christmas", edition: "12th", condition: "new", price: 100, author: "Seuss")
+  @physics_book = Book.new(isbn: "1234567891234", title: "University Physics", edition: "12th", condition: "new", price: 100, author: "Milazzo")
+  @uccs_user.books << @grinch_book
+  @ppcc_user.books << @physics_book
   @ppcc_user.save!
   @uccs_user.save!
   @uccs_user_1.save!
   @uccs_user_2.save!
+  @grinch_book.save!
+  @physics_book.save!
+  
 end
   describe "compare_ users" do
     it "return false with users from a different university" do
@@ -31,6 +38,17 @@ end
     it "return true with users from the same university" do
       log_in(@uccs_user)
       expect(compare_users(@uccs_user_2)).to eq(true)
+    end
+  end
+
+  describe "contact_info_display" do
+    it "returns no contact info if users are in a different university" do
+      log_in(@ppcc_user)
+      expect(contact_info_display(compare_users(@grinch_book.seller),@grinch_book.seller )).to eq('<p>Not same university - no contact info displayed</p>')
+    end
+    it "returns contact info if users are in the same university" do
+      log_in(@uccs_user_1)
+      expect(contact_info_display(compare_users(@grinch_book.seller),@grinch_book.seller )).to eq("<a class=\"ui basic small red button\" href=\"mailto:user@uccs.edu\">Email Seller: user@uccs.edu</a>")
     end
   end
 end
