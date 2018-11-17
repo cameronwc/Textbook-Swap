c1f4d8f9f83a94d102e7c334cf183f476708ac69
Jael Butler <jbutler6@uccs.edu>
2018-11-17 21:07:27 +0000

    Improved search book controller reek scores

--- app/controllers/search_book_controller.rb
+++ app/controllers/search_book_controller.rb
@@ -1,38 +1,40 @@
 class SearchBookController < ApplicationController
   def index
 
-	  #Get text and university fields from either params or session
-	if(params["search_text"] == "" && params["search_university"] == "")
-		@text = session["search_text"]
-		@university = session["search_university"]
+  	#Get text and university fields from either params or session
+	text = params["search_text"]
+	university = params["search_university"]
+	if(text == "" && university == "")
+		text = session["search_text"]
+		university = session["search_university"]
 	else
-		@text = params["search_text"]
-		@university = params["search_university"]
-		session["search_text"] = @text
-		session["search_university"] = @university
+		session["search_text"] = text
+		session["search_university"] = university
 	end
-	
-	@found_books = Book.find_books(params["search_text"],params["search_university"])
-	@filter = params["sort_filter"]
 
+	#Query database	
+	@found_books = Book.find_books(text,university)
+	
+	#Get number of found books
 	if(@found_books.nil? || @found_books == [])
-		@found_num = 0
+		found_num = 0
 	else
-		@found_num = @found_books.length
+		found_num = @found_books.length
 	end
-	
-	  @message = "#{@found_num} books were found"
 
-	  if(!@text.nil? && @text != "")
-		  @message = "#{@message} for search '#{@text}'"
-	  end
-	  if(!@university.nil? && @university != "")
-		  @message = "#{@message} at university '#{@university}'"
-	  end	  
+	#Initialize message to print in view
+	@message = "#{found_num} books were found"
+	if(!text.nil? && text != "")
+		@message = "#{@message} for search '#{text}'"
+	end
+	if(!university.nil? && university != "")
+		@message = "#{@message} at university '#{university}'"
+	end	  
 
-	  #Sort books if sort filter has been specified.
-	  if(!@filter.nil? && @filter != "")
-		  @found_books = Book.sort_books(@filter, @found_books)
-	  end
+	#Sort books by filter
+	filter = params["sort_filter"]
+	if(!filter.nil? && filter != "")
+		@found_books = Book.sort_books(filter, @found_books)
+	end
   end
 end
