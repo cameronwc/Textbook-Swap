72db90bd6a730bb6650359de631d489396b870ea
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-11-17 13:34:26 -0700

    fixed reek warnings in textbook_wishlist_controller

--- app/controllers/textbook_wishlist_controller.rb
+++ app/controllers/textbook_wishlist_controller.rb
@@ -1,8 +1,11 @@
+# The TextbookWishlistController is responsible for creating, destroying, and displaying contact info for wishlists.
 class TextbookWishlistController < ApplicationController
   def create
-    @wishlistCheck = Wishlist.where(:user_id => params['user_id'], :book_id => params['book_id'])
-    if(@wishlistCheck.length == 0)
-      @new_wishlist = Wishlist.create(:user_id => params['user_id'], :book_id => params['book_id'])
+    user_id = params['user_id'];
+    book_id = params['book_id'];
+    @check_for_existence = Wishlist.where(:user_id => user_id, :book_id => book_id)
+    if(@check_for_existence.length == 0)
+      @new_wishlist = Wishlist.create(:user_id => user_id, :book_id => book_id)
     end
     redirect_to "/"
   end
