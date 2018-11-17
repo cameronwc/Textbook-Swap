f997edaad2f4a8caf79358019129096794285ffd
EjRuacho <36456555+EjRuacho@users.noreply.github.com>
2018-11-17 14:39:01 -0700

    Fixed a few warnings in the book.rb in the Models folder.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
+#Responsible for handling the Book model such as sorting findings through the search feature
 class Book < ApplicationRecord
     belongs_to :seller, class_name: 'Account', foreign_key: 'account_id'
     validates :isbn, presence: true, length: {minimum:9, maximum: 14 }
@@ -21,35 +22,30 @@ class Book < ApplicationRecord
 
         filter = filter.downcase
         if filter == "condition"
-            #new , good, fair , poor
-            new_books = Array.new
-            good_books = Array.new
-            fair_books = Array.new
-            poor_books = Array.new
-            books.each do |x|
-                if x.condition.downcase == "new"
-                    new_books.push(x)
-                elsif x.condition.downcase == "good"
-                    good_books.push(x)
-                elsif x.condition.downcase == "fair"
-                    fair_books.push(x)
-                elsif x.condition.downcase == "poor"
-                    poor_books.push(x)
-                end
-            end
-            sorted_books = new_books.append(good_books).append(fair_books).append(poor_books).flatten!
-            return sorted_books
-
+            return sorted_books = sort_by_condition(books)
         elsif filter == "edition"
-            sorted_books = books.sort_by {|book| book.edition.gsub(/\D/,'')}.reverse!
-            return sorted_books
-
+            return sorted_books = books.sort_by {|book| book.edition.gsub(/\D/,'')}.reverse!
         elsif filter == "price"
-            sorted_books = books.sort {|x, y| x.price <=> y.price}
-            return sorted_books
-
+            return sorted_books = books.sort {|firstbook, secondbook| firstbook.price <=> secondbook.price}
         else
             return books
         end
+    end
+
+    def sort_by_condition(books)
+        new_books = Array.new,good_books = Array.new,fair_books = Array.new,poor_books = Array.new
+        books.each do |book|
+            condition = book.condition.downcase
+            if condition == "new"
+                new_books.push(x)
+            elsif condition == "good"
+                good_books.push(x)
+            elsif condition == "fair"
+                fair_books.push(x)
+            elsif condition == "poor"
+                poor_books.push(x)
+            end
+        end
+        return sorted_books = new_books.append(good_books).append(fair_books).append(poor_books).flatten!
     end
 end
