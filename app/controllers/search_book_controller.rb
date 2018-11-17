5cbf3e35c4dfcff35c72629923a52ed9f7cd04a5
Jael Butler <jbutler6@uccs.edu>
2018-11-17 22:11:20 +0000

    updated controller/helper

--- app/controllers/search_book_controller.rb
+++ app/controllers/search_book_controller.rb
@@ -1,30 +1,29 @@
+#The search/sort book controller for book searches/sorts
 class SearchBookController < ApplicationController
 
   def index
-
   	#Get text and university fields from either params or session
-	text = params["search_text"]
-	university = params["search_university"]
-	if(text == "" && university == "")
-		text = session["search_text"]
-		university = session["search_university"]
-	else
-		session["search_text"] = text
-		session["search_university"] = university
-	end
+#	text = params["search_text"]
+#	university = params["search_university"]
+#	if(text == "" && university == "")
+#		text = session["search_text"]
+#		university = session["search_university"]
+#	else
+#		session["search_text"] = text
+#		session["search_university"] = university
+#	end
+	text = helpers.set_text
+	university = helpers.set_university
 
 	#Query database	
 	@found_books = Book.find_books(text,university)
 	
-	#Get number of found books
-	found_num = @found_books.length
-
 	#Initialize message to print in view
-	@message = helpers.set_message(found_num, text, university)
+	@message = helpers.set_message(@found_books.length, text, university)
 
 	#Sort books by filter
 	filter = params["sort_filter"]
-	if(!filter.nil? && filter != "")
+	if(!filter.nil?)
 		@found_books = Book.sort_books(filter, @found_books)
 	end
   end
--- app/helpers/search_book_helper.rb
+++ app/helpers/search_book_helper.rb
@@ -2,11 +2,7 @@ module SearchBookHelper
 
 	#Sets the message to print in the search view based on search parameters.
 	def set_message(number, search_text, search_university)
-		if(number == 1)
-			message = "1 book found"
-		else
-			message = "#{number} books found"
-		end
+		message = set_num_message(number)
 		if(search_text != "")
 			message = "#{message} for search '#{search_text}'"
 		end
@@ -14,5 +10,37 @@ module SearchBookHelper
 			message = "#{message} at university '#{search_university}'"
 		end
 		return message
+	end
+
+	#Grammar helper for message depending on number of books found
+	def set_num_message(number)
+		if(number == 1)
+			message = "1 book found"
+		else
+			message = "#{number} books found"
+		end
+		return message
+	end
+
+	#Sets the search text from params/session hash
+	def set_text
+		text = params["search_text"]
+		if(text != "")
+			session["search_text"] = text
+		else
+			text = session["search_text"]
+		end
+		return text
+	end
+
+	#Sets the search university from params/session hash
+	def set_university
+		university = params["search_university"]
+		if(university != "")
+			session["search_university"] = university
+		else
+			university = session["search_university"]
+		end
+		return university
 	end
 end
