2fc57e5d44767daa1fe8d6ff7a9bc2ab6c477463
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-11-17 13:57:37 -0700

    refactored add_book_controller

--- app/controllers/add_book_controller.rb
+++ app/controllers/add_book_controller.rb
@@ -1,26 +1,20 @@
+# The AddBookController is responsible for rendering and creating books on the site. This class also has a similar function to return similar books by isbn.
 class AddBookController < ApplicationController
+  before_action :login_check, :except => [:similar]
+
   def index
-    if logged_in?
-      @LogInOrOut = "Logout, " + String(@current_user.name)
-    else
-      redirect_to "/login"
-    end
   end
 
   def create
-    if logged_in?
-      @createdBook = Book.create(:isbn => params['isbn'], :title => params['title'], :edition => params['edition'], :author => params['author'], :condition => params['condition'], :price => params['price'])
-      @createdBook.seller = @current_user
-      if (@createdBook.valid?)
-        @createdBook.save!
-        @current_user.books << @createdBook
-        redirect_to "/dashboard"
-      else
-        flash.alert = "Please check your fields."
-        redirect_to "/add_book"
-      end
+    @createdBook = Book.create(:isbn => params['isbn'], :title => params['title'], :edition => params['edition'], :author => params['author'], :condition => params['condition'], :price => params['price'])
+    @createdBook.seller = @current_user
+    if (@createdBook.valid?)
+      @createdBook.save!
+      @current_user.books << @createdBook
+      redirect_to "/dashboard"
     else
-      redirect_to "/login"
+      flash.alert = "Please check your fields."
+      redirect_to "/add_book"
     end
   end
 
@@ -29,3 +23,12 @@ class AddBookController < ApplicationController
     render :json => @returnedBooks.to_json
   end
 end
+
+def login_check
+  if !logged_in?
+    redirect_to "/login"
+  else
+    @LogInOrOut = "Logout, " + String(@current_user.name)
+  end
+end
+
