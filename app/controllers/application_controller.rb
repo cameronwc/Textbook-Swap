45cf182b2bdfef42ea9dccf80a420622988cf2cd
Josh Willis <jwillis2@uccs.edu>
2018-11-17 17:55:41 -0700

    improved reek and flog for login helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+#LoginHelper is included so that the functions included can be used anywhere in the application
 class ApplicationController < ActionController::Base
     #protect_from_forgery with: :null_session
     skip_before_action :verify_authenticity_token
--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,4 +1,11 @@
 require 'sanitize'
+# The LoginHelper has many functions that supplement the LoginController including
+# log_in - performs the fundamental task of a log in - sets session variable
+# login_compare - helps the check function in the controller determine what to do when the password is either correct or incorrect
+# logged_in? - checks if a user is logged in
+# compare_users - compares a user with the logged in user to see if thy are from the same university
+# contact_info_display
+
 module LoginHelper
   #require 'sanitize'
   def log_in(user)
@@ -15,8 +22,9 @@ module LoginHelper
     end
   end
   def current_user
-    if session[:user_id]
-      @current_user ||= Account.find_by(id: session[:user_id])
+    session_user = session[:user_id]
+    if session_user
+      @current_user ||= Account.find_by(id: session_user)
     end
   end
   def logged_in?
@@ -30,7 +38,7 @@ module LoginHelper
   def compare_users(book_seller)
     @current_user = current_user
     seller_domain = book_seller.email[/(?<=@)[^.]+(?=\.)/].to_s.downcase
-    if !@current_user.nil?
+    if logged_in?
     buyer_domain = @current_user.email[/(?<=@)[^.]+(?=\.)/].to_s.downcase
     end
     if seller_domain == buyer_domain
@@ -39,17 +47,16 @@ module LoginHelper
       return false
     end
   end
-  def contact_info_display(compare, book_seller)
-    format1="<a class='ui basic small green button' href='mailto:#{book_seller.email}'>Email Seller: #{book_seller.email}</a>"
-    format2='<p>Not same university - no contact info displayed</p>'
-    format3='<a href="/login">Please login to view email</a>'
-
+  def contact_info_display(compare, email)
+    demail="<a class='ui basic small green button' href='mailto:#{email}'>Email Seller: #{email}</a>"
+    dmessage='<p>Not same university - no contact info displayed</p>'
+    dlogin='<a href="/login">Please login to view email</a>'
     if logged_in? && compare
-      return Sanitize.fragment(format1, Sanitize::Config::RELAXED)
+      return Sanitize.fragment(demail, Sanitize::Config::RELAXED)
     elsif logged_in? && !compare
-      return Sanitize.fragment(format2, Sanitize::Config::RELAXED)
+      return Sanitize.fragment(dmessage, Sanitize::Config::RELAXED)
     else
-      return Sanitize.fragment(format3, Sanitize::Config::RELAXED)
+      return Sanitize.fragment(dlogin, Sanitize::Config::RELAXED)
     end
   end
 
